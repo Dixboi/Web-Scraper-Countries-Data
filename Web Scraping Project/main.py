2a03ee8c1ee8bbd8cfb6194b004bd89dde87cbb7
Dex <59426458+Dixboi@users.noreply.github.com>
2020-11-02 16:15:51 +0800

    Update main.py

--- Web Scraping Project/main.py
+++ Web Scraping Project/main.py
@@ -1,5 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+import requests #pip install requests
+from bs4 import BeautifulSoup #pip install beautifulsoup4
 import webScraperModule as wsm
 
 if __name__ == "__main__":
@@ -14,4 +14,34 @@ if __name__ == "__main__":
   all_areas = soup.find_all(class_ = 'country-area')
 
   wsm.instructions()
-
+  user = "Python:\wsm\command>"
+  
+  while True:
+    command = input(user).split()
+    if command[0] == "//term":
+      wsm.exitProgram()
+    elif command[0] != "//term":
+      try:
+        if command[0] == "//a":
+          wsm.displayAllInfos(all_names, all_capitals, all_populations, all_areas)
+        elif command[0] == "//gen": 
+          wsm.findSpecificInfoGENERAL(command[1], all_names, all_capitals, all_populations, all_areas)
+        elif command[0] == "//co":
+          wsm.findSpecificInfoNAMES(command[1], all_names, all_capitals, all_populations, all_areas)
+        elif command[0] == "//ca":
+          wsm.findSpecificInfoCAPITALS(command[1], all_names, all_capitals, all_populations, all_areas)
+        elif command[0] == "//pop":
+          wsm.findSpecificInfoPOPULATIONS(command[1], command[2], all_names, all_capitals, all_populations, all_areas)
+        elif command[0] == "//ar":
+          wsm.findSpecificInfoAREAS(command[1], command[2], all_names, all_capitals, all_populations, all_areas)
+        elif command[0] == "//clear":
+          wsm.clearScreen()
+        elif command[0] == "//hlp":
+          wsm.instructions()
+        else:
+          command = " ".join(command)
+          print(f'"{command}" is not recognized as a valid command in this program.')
+      except:
+        print("Invalid entry.")
+    else:
+      print(f'"{command}" is not recognized as a valid command in this program.')
